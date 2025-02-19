7d1095bf12e70fbebdd7bbe086878d558b2a4f56
Tidris34 <tidris1453@gmail.com>
2025-02-19 13:00:38 -0800

    monday work

--- main.cpp
+++ main.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 int main() {
-    unsigned int rows = 3;  // Corrected number of rows
-    unsigned int cols = 3;
+    unsigned int rows = 5;  // Corrected number of rows
+    unsigned int cols = 5;
 
-    const bool* maze[3] = {
-        new bool[3]{false, false, true},
-        new bool[3]{false, true, true},
-        new bool[3]{false, true, false}
+    const bool* maze[rows] = {
+        new bool[cols]{false, false, true, false,true},
+        new bool[cols]{false, true, true, false, false},
+        new bool[cols]{false, false, false, true, false},
+        new bool[cols]{false, false, true, false, false},
+        new bool[cols]{false, false, false, false, false},
     };
 
     Maze maze1((const bool**)maze, rows, cols);  // Explicit cast
+    Location start(3, 2);
 
     maze1.print_maze();
 
+    //cout << maze1.can_escape(start) ? "yes it can": "no it can't";
     // Free dynamically allocated memory
     for (int i = 0; i < rows; ++i) {
         delete[] maze[i];
--- maze.cpp
+++ maze.cpp
@@ -1,13 +1,36 @@
 #include "maze.h"
+#include <functional>
 
 using namespace std;
 
 bool Maze::can_escape(Location start){
+    if(!maze[start.first][start.second]){
+        return false;
+    }
+    if (
+        start.first == 0 || 
+        start.first == num_rows - 1 || 
+        start.second == 0 || 
+        start.second == num_cols - 1
+    ) {
+        return true;
+    }
+    else if(
+        can_escape(Location(start.first + 1, start.second)) ||
+        can_escape(Location(start.first - 1, start.second)) ||
+        can_escape(Location(start.first, start.second + 1)) ||
+        can_escape(Location(start.first, start.second - 1))
+    ){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
     return false;
 }
 
-vector<Location>Maze::escape_path(Location start){
-    
-    return; 
-
+vector<Location> Maze::escape_path(Location start) {
+ 
+     
 }
